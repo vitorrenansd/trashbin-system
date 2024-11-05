c859e3607dd964df91b6ddbd6711497408dfb5e4
Vitor <167242612+VitorRenanSD@users.noreply.github.com>
2024-11-04 21:07:52 -0300

    Update Trashbin.java

--- src/Trashbin.java
+++ src/Trashbin.java
@@ -3,7 +3,9 @@ public class Trashbin {
     private String location;
     private int clean;
     private String status;
+    private int fullness;
 
+    
     // Constructor
     public void Trashbin(int height, String location) {
         this.height = height;
@@ -13,16 +15,30 @@ public class Trashbin {
     }
 
     // Get the % of fullness
-    public int getPercentage(int soundReturn) {
-        return (this.height - soundReturn);
+    public void getPercentage(int soundReturn) {
+        this.fullness = this.height - soundReturn;
     }
 
-    // Return the status of the trash bin with %
+    // Cleans the trashbin
+    public void cleanTrashbin() {
+        this.status = "empty";
+        this.fullness = 0;
+        this.clean += 1;
+    }
+    
+    // Change status of the trash bin
     public void scanTrashbin(int trash) {
-        if (getPercentage(trash) >= this.height - (this.height / 100 * 90)) {
-
+        if (this.fullness >= this.height / 100 * 80) {
+            this.status = "Full";
+        }
+        if (this.fullness <= this.height / 100 * 20) {
+            this.status = "Empty";
+        } else {
+            this.status = "Half Empty";
         }
     }
-
-
+    
+    public void printTrashbin() {
+        System.out.println(this.status + " | " + this.location + " | " + this.clean + " | " + getPercentage());
+    }
 }
